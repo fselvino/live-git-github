071e66087c50395c9e12f3ff49cf43575cc2cb1f
fernandoselvino@gmail.com <fernandoselvino@gmail.com>
2019-08-17 21:21:13 -0300

    chore: Terceiro commint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res, next) => {
-  return res.json({ hello: "Wold 2" });
+  return res.json({ hello: "Wold 2 3" });
 });
 
 app.listen(3000);
